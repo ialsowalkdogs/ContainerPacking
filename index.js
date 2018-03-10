e0af8036310152758d8ff469ffca883c2b5b8fbb
Olga Vorozheykina <solariskind@gmail.com>
2018-03-10 18:16:48 +0200

    Implemented fit by volume

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ function Container(width, height, length) {
     this.width = width;
     this.height = height;
     this.length = length;
-    this.volume = function () {
-        return this.width * this.height * this.length;
-    };
+    this.volume = width * height * length;
 }
 
 const smallBag = new Container(16, 23, 2);
 const mediumBag = new Container(22, 26, 2);
 const largeBag = new Container(14, 26, 10);
 
-// Nulling the values
+// Initialising values
 let totalBags = 0;
+let totalBagsVolume = 0;
 let smallBags = 0;
 let mediumBags = 0;
 let largeBags = 0;
 let bags = [];
+
 let contSize = 0;
 let container = {};
 let containers = [];
 
 function assign() {
+    //Emptying arrays
+    bags.length = 0;
+    containers.length = 0;
+
     smallBags = document.getElementById("small").value;
     mediumBags = document.getElementById("medium").value;
     largeBags = document.getElementById("large").value;
@@ -41,23 +45,22 @@ function assign() {
     }
 
     totalBags = bags.length;
+    totalBagsVolume = bags.reduce(function(sum, current) {
+        return sum + current.volume;
+      }, 0);
 
-    contSize = document.getElementById("contSize").value;
-    if (!contSize.match(/^[0-9]+$/)) {
-        alert("Must input numbers! Enter a number between 30 and 100");
-    }    
-    else if (contSize < 30 || contSize > 100) {
+    contSize = Number(document.getElementById("contSize").value);
+    if (contSize < 30 || contSize > 100) {
         alert("Invalid number! Number must be between 30 and 100");
     }
     else {
         container = new Container(contSize, contSize, contSize);
-    };    
-    
+    }        
 }
 
 // Initialise an instance of a container
 function addNewContainer() {
-    containers.push(container);
+    containers.push(new Container(contSize, contSize, contSize));
 }
 
 // Main algorythm - First Fit Decreasing
@@ -76,23 +79,30 @@ function fitBoxes() {
 
     let bag = bags.pop();
 
-    for (var i = 0; i < containers.length; i++) {
-        if (containers[i].volume >= bag.volume) {
-            containers[i].volume -= bag.volume;
-            return;
+    let fitContainer = -1;
+        // Find first fitting container
+        for (var j = 0; j < containers.length; j++) {
+            if (containers[j].volume >= bag.volume) {
+                fitContainer = j;
+                break;
+            }          
         }
-
+        // If it's found, box is assigned to it
+        if (fitContainer != -1) {
+            containers[fitContainer].volume -= bag.volume;
+        }
+        // Else add a new container and assign box to it 
         else {
             addNewContainer();
-            containers[containers.length - 1] -= bag.volume;
-            fitBoxes();
+            containers[containers.length - 1].volume -= bag.volume;
         }
-        // TODO: check a box for dimensional fit 
-    }
+
+        fitBoxes();
 }
 
 function output() {
-    document.getElementById("output").innerHTML = `For ${totalBags} bags we will need at least ${containers.length} boxes.`;
+    document.getElementById("output").innerHTML = `<p>The volume of one container is ${container.volume} cm3.</p>
+    We packed ${totalBags} bags with total volume of ${totalBagsVolume} cm3. For that we will need at least ${containers.length} boxes.`;
 }
 
 function main() {
